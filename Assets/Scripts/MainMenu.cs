8f71923f33eaf71cdfe8c7ed6d25224a7e299ead
RafalNiemiec <rafal.niemiec1998@gmail.com>
2020-06-03 00:18:58 +0200

    Add working menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void Test()
+    {
+        print("Quit!");
+        Application.Quit();
+    }
+}
